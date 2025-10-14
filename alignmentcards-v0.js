7e84277a65de2b5122e043424e36781f73b079ec
ramlukn <nikhilr.ramlukan@gmail.com>
2025-10-14 12:34:26 -0400

    Update alignmentcards-v0.js

--- alignmentcards-v0.js
+++ alignmentcards-v0.js
@@ -7,7 +7,7 @@
     "color": "#E6FFE9",
     "description": "Alignment principles are contestable, general-purpose, broadly recognized ethical or social or normative commitments that can serve as warrants for recommending or evaluating an agent's course of action in contexts where alignment and cooperation with others matters."
   },
-  {
+   {
     "code": "AB", 
     "name": "Hoping for A, Rewarding B", 
     "pathology": "lorem ipsum", 
@@ -153,4 +153,5 @@
        "professional": "Experts misreport or give biased findings to satisfy stakeholders",
        "machine": "AI 'hallucinates' to satisfy the user"
      }
+   },
 ]
